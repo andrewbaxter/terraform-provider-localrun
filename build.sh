1e4cabdcce5a1caecda402fd96bee9745b87649f
andrew <>
2022-12-28 22:11:16 +0900

    Docs

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash -xeu
 rm -rf docs
-go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name localrun --rendered-provider-name "Local Run" --rendered-website-dir docs
+go run -modfile tools.mod github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name localrun --rendered-provider-name "Local Run" --rendered-website-dir docs
